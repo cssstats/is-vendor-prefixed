94354361dd02f8639fcd3ba22c5d366531efcfca
John Otander <johnotander@gmail.com>
2015-01-20 13:43:09 -0700

    Implement isVendorPrefixed

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 'use strict';
 
-module.exports = function isVendorPrefixed(options) {
-  options = options || {};
+var vendorPrefixes = require('vendor-prefixes');
 
-  return true;
+module.exports = function isVendorPrefixed(property) {
+  if (typeof property != 'string') {
+    throw new TypeError('is-vendor-prefixed expected a string');
+  }
+
+  var regexForPrefixes = new RegExp('^(' + vendorPrefixes().join('|') + ')([a-z\-]+)$', 'i');
+  return regexForPrefixes.test(property);
 }
